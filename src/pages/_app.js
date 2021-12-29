47e6a7094605b93897f55fea7a83d93ea786f870
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2021-12-29 14:27:37 +0200

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import Header from '../components/Header/Header';
 import { Container } from '../components/Layout/LayoutStyles';
-
 import Theme from '../styles/theme';
-
 export default function App({ Component, pageProps }) {
   return (
     <>
